0a41c69c3ebd8e595205e21bce6c5a99c5a856a9
Z-One <trannhatthanh31@gmail.com>
2021-08-28 18:16:30 +0700

    Init Source.

---
+++ src/main/java/com/laundy/laundrybackend/LaundryBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.laundy.laundrybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaundryBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaundryBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/laundy/laundrybackend/LaundryBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laundy.laundrybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaundryBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
