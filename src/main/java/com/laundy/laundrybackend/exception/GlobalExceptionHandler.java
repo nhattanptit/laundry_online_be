f286c187e7e63b85fe1fb5a7ad21ca1701cfea55
Z-One <trannhatthanh31@gmail.com>
2021-08-29 22:23:21 +0700

    Finish login and security config.

--- src/main/java/com/laundy/laundrybackend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/laundy/laundrybackend/exception/GlobalExceptionHandler.java
@@ -6,14 +6,13 @@ import com.laundy.laundrybackend.models.response.GeneralResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.validation.ValidationException;
 
-//@ControllerAdvice
+@ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler({Exception.class})
