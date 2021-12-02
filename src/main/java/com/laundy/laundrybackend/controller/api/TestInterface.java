339fb0e155b8b6010dbc673d66fd7dd54b927c6d
thanhtrannhat <thanh.trannhat@nttdata.com>
2021-12-02 10:14:32 +0700

    Added TestController

--- src/main/java/com/laundy/laundrybackend/controller/api/TestInterface.java
+++ src/main/java/com/laundy/laundrybackend/controller/api/TestInterface.java
@@ -2,10 +2,12 @@ package com.laundy.laundrybackend.controller.api;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("test/")
 @CrossOrigin("*")
 public interface TestInterface {
+    @GetMapping("test")
     ResponseEntity<?> testTerra();
 }
